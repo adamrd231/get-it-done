fbe7c4dd36415b7bd4f31ab9fabdd540517224cc
Adam Reed <adam.rdconcepts@gmail.com>
2017-06-23 19:58:26 -0700

    create template files for get it done

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+tasks = []
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+    return render_template('todos.html',
+          title="Get Er' Done",
+          tasks=tasks)
+
+app.run()
