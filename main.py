f1e139417f2955a288fc6812a5dd621c3a3b46c8
Adam Reed <adam.rdconcepts@gmail.com>
2017-06-24 00:07:40 -0700

    finish part 6

--- main.py
+++ main.py
@@ -20,6 +20,55 @@ class Task(db.Model):
         self.completed = False
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            # to do - "remember that the user has logged in"
+            return redirect('/')
+        else:
+            return '<h1>Failed the contest.</h1>'
+
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # - Validate the users data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            #todo - Remember the user
+            return redirect('/')
+        else:
+            # todo - user better response message
+            return "<h1>Duplicate User</h1>"
+
+    return render_template('register.html')
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
